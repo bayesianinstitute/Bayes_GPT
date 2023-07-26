c07b7cb1add58dad70e84340943ca47c9de3c678
faizack <faizack619@gmail.com>
2023-07-26 04:58:56 -0700

    remove stream of charaters

--- client/src/components/content/chat.jsx
+++ client/src/components/content/chat.jsx
@@ -1,146 +1,133 @@
 import React, {
   forwardRef,
   Fragment,
-  useImperativeHandle,
-  useRef,
-} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { GptIcon } from "../../assets";
-import { RobotIcon } from "../../assets";
+  useImperativeHandle, useRef
+} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { GptIcon } from '../../assets'
+import { RobotIcon } from '../../assets'
 
-import { insertNew } from "../../redux/messages";
-import "./style.scss";
-import ReactMarkdown from "react-markdown";
-
-
+import { insertNew } from '../../redux/messages'
+import './style.scss'
+import ReactMarkdown from 'react-markdown'; 
+import ReactDOMServer from 'react-dom/server';
 const Chat = forwardRef(({ error }, ref) => {
-  const dispatch = useDispatch();
 
-  const contentRef = useRef();
+  const dispatch = useDispatch()
 
-  const { user, messages } = useSelector((state) => state);
-  const { latest, content, all } = messages;
+  const contentRef = useRef()
+
+  const { user, messages } = useSelector((state) => state)
+  const { latest, content, all } = messages
 
   const renderMarkdown = (content) => {
-    return ReactMarkdown.render(content);
+    return ReactDOMServer.renderToString(<ReactMarkdown>{content}</ReactMarkdown>);
   };
-
-  const loadResponse = (
-    stateAction,
-    response = content,
-    chatsId = latest?.id
-  ) => {
+  
+  const loadResponse = (stateAction, response = content, chatsId = latest?.id) => {
     clearInterval(window.interval);
 
     stateAction({ type: "resume", status: true });
 
     contentRef?.current?.classList?.add("blink");
 
-    let index = 0;
+    let renderedContent = renderMarkdown(response); // Render the entire content at once
 
-    window.interval = setInterval(() => {
-      if (index < response.length && contentRef?.current) {
-        if (index === 0) {
-          dispatch(insertNew({ chatsId, content: response.charAt(index) }));
-          contentRef.current.innerHTML =  response.charAt(index);
-        } else {
-          dispatch(
-            insertNew({
-              chatsId,
-              content: response.charAt(index),
-              resume: true,
-            })
-          );
-          contentRef.current.innerHTML += response.charAt(index);
-        }
-        index++;
-      } else {
-        stopResponse(stateAction);
-      }
-    }, 20);
+    contentRef.current.innerHTML = `<span style="display: inline-block">${renderedContent}</span>`;
+    dispatch(insertNew({ chatsId, content: response, resume: true }));
+
+    stopResponse(stateAction);
   };
-
+  
   const stopResponse = (stateAction) => {
     if (contentRef?.current) {
-      contentRef.current.classList.remove("blink");
+      contentRef.current.classList.remove('blink')
     }
-    stateAction({ type: "resume", status: false });
-    clearInterval(window.interval);
-  };
+    stateAction({ type: 'resume', status: false })
+    clearInterval(window.interval)
+  }
 
   useImperativeHandle(ref, () => ({
     stopResponse,
     loadResponse,
     clearResponse: () => {
       if (contentRef?.current) {
-        contentRef.current.innerHTML = "";
-        contentRef?.current?.classList.add("blink");
+        contentRef.current.innerHTML = ''
+        contentRef?.current?.classList.add("blink")
       }
-    },
-  }));
+    }
+  }))
 
   return (
-    <div className="Chat">
-      {all
-        ?.filter((obj) => {
-          return !obj.id ? true : obj?.id !== latest?.id;
-        })
-        ?.map((obj, key) => {
+    <div className='Chat'>
+      {
+        all?.filter((obj) => {
+          return !obj.id ? true : obj?.id !== latest?.id
+        })?.map((obj, key) => {
           return (
             <Fragment key={key}>
-              <div className="qs">
-                <div className="acc">{user?.fName?.charAt(0)}</div>
-                <div className="txt1">
-                  <ReactMarkdown>{obj?.prompt}</ReactMarkdown>
+              <div className='qs'>
+                <div className='acc'>
+                  {user?.fName?.charAt(0)}
+                </div>
+                <div className='txt1'>
+                <ReactMarkdown>{obj?.prompt}</ReactMarkdown>
                 </div>
               </div>
 
               <div className="res">
-                <div className="icon">
+                <div className='icon'>
                   <RobotIcon />
                   {/*<GptIcon />*/}
                 </div>
-                <div className="txt">
+                <div className='txt'>
                   <span>
-                    <ReactMarkdown>{obj?.content}</ReactMarkdown>
+                   <ReactMarkdown>
+                    {obj?.content}
+                    </ReactMarkdown>
                   </span>
                 </div>
               </div>
             </Fragment>
-          );
-        })}
+          )
+        })
+      }
 
-      {latest?.prompt?.length > 0 && (
-        <Fragment>
-          <div className="qs">
-            <div className="acc">{user?.fName?.charAt(0)}</div>
-            <div className="txt">
-              <ReactMarkdown>{latest?.prompt}</ReactMarkdown>
+      {
+        latest?.prompt?.length > 0 && (
+          <Fragment>
+            <div className='qs'>
+              <div className='acc'>
+                {user?.fName?.charAt(0)}
+              </div>
+              <div className='txt'>
+              <ReactMarkdown>
+                {latest?.prompt}
+                </ReactMarkdown>
+              </div>
             </div>
-          </div>
 
-          <div className="res">
-            <div className="icon">
-              <RobotIcon />
+            <div className="res">
+              <div className='icon'>
+                <RobotIcon />
 
-              {/*<GptIcon />*/}
+                {/*<GptIcon />*/}
+             
 
-              {error && <span>!</span>}
+                {error && <span>!</span>}
+              </div>
+              <div className='txt'>
+                {
+                  error ? <div className="error">
+                    Something went wrong. If this issue persists please contact us through our help center at help.openai.com.
+                  </div> : <span ref={contentRef} className="blink" />
+                }
+              </div>
             </div>
-            <div className="txt">
-                {error ? (
-                  <div className="error">
-                    Something went wrong. If this issue persists please contact
-                    us through our help center at help.openai.com.
-                  </div>
-                ) : (
-                  <span ref={contentRef} className="blink" />
-                )}
-            </div>
-          </div>
-        </Fragment>
-      )}
+          </Fragment>
+        )
+      }
     </div>
-  );
-});
-export default Chat;
+  )
+})
+export default Chat
