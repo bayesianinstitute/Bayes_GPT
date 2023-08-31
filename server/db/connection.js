9ca34d883d2d2e91d83142ea590aaa44b8418c0a
faizack <faizack619@gmail.com>
2023-08-31 21:48:30 +0530

    added mongodb and fix button issue

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,7 @@ let db = null
 const connectDB = async (done) => {
     try {
         var data = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true })
-        db = data.db('chatgpt')
+        db = data.db('E-commerce_chatgpt')
         done()
     } catch (err) {
         done(err)
