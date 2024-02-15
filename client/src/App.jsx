50064748bd5179648a7547cf75cc0ed91cc897f0
Faizack <faizack619@gmail.com>
2024-02-16 01:22:50 +0530

    lazy in router to optimize the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState, Suspense } from "react";
 import { Menu } from "./components";
-import { Routes, Route } from "react-router-dom";
-import { Error, Forgot, Login, Main, Signup } from "./page";
+import { Routes, Route, } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProtectedRoute from "./protected";
 import Loading from "./components/loading/loading";
+import { lazy } from "react";
+
+const Main = lazy(() => import("./page/chat"));
+const Login = lazy(() => import("./page/Login"));
+const Signup = lazy(() => import("./page/Signup"));
+const Forgot = lazy(() => import("./page/Forgot"));
+const ErrorPage = lazy(() => import("./page/Error"));
 
 const App = () => {
   const [offline, setOffline] = useState(!window.navigator.onLine);
@@ -14,11 +20,9 @@ const App = () => {
   const changeColorMode = (to) => {
     if (to) {
       localStorage.setItem("darkMode", true);
-
       document.body.className = "dark";
     } else {
       localStorage.removeItem("darkMode");
-
       document.body.className = "light";
     }
   };
@@ -26,7 +30,6 @@ const App = () => {
   // Dark & Light Mode
   useLayoutEffect(() => {
     let mode = localStorage.getItem("darkMode");
-
     if (mode) {
       changeColorMode(true);
     } else {
@@ -36,14 +39,23 @@ const App = () => {
 
   // Offline
   useEffect(() => {
-    window.addEventListener("online", (e) => {
+    window.addEventListener("online", () => {
       location.reload();
     });
 
-    window.addEventListener("offline", (e) => {
+    window.addEventListener("offline", () => {
       setOffline(true);
     });
-  });
+
+    return () => {
+      window.removeEventListener("online", () => {
+        location.reload();
+      });
+      window.removeEventListener("offline", () => {
+        setOffline(true);
+      });
+    };
+  }, []);
 
   return (
     <section className={user ? "main-grid" : null}>
@@ -56,35 +68,36 @@ const App = () => {
       {loading && <Loading />}
 
       {offline && (
-        <Error
+        <ErrorPage
           status={503}
           content={"Website in offline check your network."}
         />
       )}
 
-      <Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<ProtectedRoute offline={offline} authed={true} />}>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/chat" element={<Main />} />
+            <Route path="/chat/:id" element={<Main />} />
+          </Route>
 
-        <Route element={<ProtectedRoute offline={offline} authed={true} />}>
-          <Route exact path="/" element={<Main />} />
-          <Route path="/chat" element={<Main />} />
-          <Route path="/chat/:id" element={<Main />} />
-        </Route>
-
-        <Route element={<ProtectedRoute offline={offline} />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/login/auth" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signup/pending/:id" element={<Signup />} />
-          <Route path="/forgot" element={<Forgot />} />
-          <Route path="/forgot/set/:userId/:secret" element={<Forgot />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <Error status={404} content={"This page could not be found."} />
-          }
-        />
-      </Routes>
+          <Route element={<ProtectedRoute offline={offline} />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/login/auth" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signup/pending/:id" element={<Signup />} />
+            <Route path="/forgot" element={<Forgot />} />
+            <Route path="/forgot/set/:userId/:secret" element={<Forgot />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <ErrorPage status={404} content={"This page could not be found."} />
+            }
+          />
+        </Routes>
+      </Suspense>
     </section>
   );
 };
